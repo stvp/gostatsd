e64e62aebc08741a9452544553ee89176ae5f93c
Tyson Tate <tyson@tysontate.com>
2013-03-28 10:26:47 -0700

    Use go-udp-testing for UDP testing.

--- statsd_test.go
+++ statsd_test.go
@@ -1,43 +1,11 @@
 package statsd
 
 import (
-	"net"
+	"github.com/stvp/go-udp-testing"
 	"reflect"
 	"testing"
 	"time"
 )
-
-// -- Helpers
-
-var addr, _ = net.ResolveUDPAddr("udp", ":8125")
-var listener, _ = net.ListenUDP("udp", addr)
-
-type fn func()
-
-// Expect to receive the given message on UDP port 8125 while running the given
-// func.
-func expectMessage(t *testing.T, expected string, body fn) {
-	got := getMessage(body)
-
-	// Check the actual received bytes
-	if got != expected {
-		t.Errorf("Expected %#v but got %#v instead.", expected, got)
-	}
-}
-
-// Get the UDP data received on port 8125 while running the given function.
-func getMessage(body fn) string {
-	result := make(chan string)
-	go func() {
-		message := make([]byte, 1024)
-		n, _, _ := listener.ReadFrom(message)
-		result <- string(message[0:n])
-	}()
-
-	body()
-
-	return <-result
-}
 
 // Return a valid client that sends messages to the server set up above.
 func goodClient(prefix string) StatsReporter {
@@ -66,20 +34,22 @@ func TestNew(t *testing.T) {
 }
 
 func TestGauge(t *testing.T) {
+	udp.SetAddr(":8125")
+
 	// Positive numbers
-	expectMessage(t, "bukkit:2|g", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:2|g", func() {
 		client := goodClient("")
 		client.Gauge("bukkit", 2)
 		client.Flush()
 	})
 	// Negative numbers
-	expectMessage(t, "bukkit:-12|g", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:-12|g", func() {
 		client := goodClient("")
 		client.Gauge("bukkit", -12)
 		client.Flush()
 	})
 	// Large floats
-	expectMessage(t, "bukkit:1.234567890123457|g", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:1.234567890123457|g", func() {
 		client := goodClient("")
 		client.Gauge("bukkit", 1.2345678901234568901234)
 		client.Flush()
@@ -87,26 +57,28 @@ func TestGauge(t *testing.T) {
 }
 
 func TestCount(t *testing.T) {
+	udp.SetAddr(":8125")
+
 	// Positive numbers
-	expectMessage(t, "bukkit:2|c", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:2|c", func() {
 		client := goodClient("")
 		client.Count("bukkit", 2, 1)
 		client.Flush()
 	})
 	// Negative numbers
-	expectMessage(t, "bukkit:-10|c", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:-10|c", func() {
 		client := goodClient("")
 		client.Count("bukkit", -10, 1)
 		client.Flush()
 	})
 	// Large floats
-	expectMessage(t, "bukkit:1.234567890123457|c", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:1.234567890123457|c", func() {
 		client := goodClient("")
 		client.Count("bukkit", 1.2345678901234568901234, 1)
 		client.Flush()
 	})
 	// Sample rates
-	expectMessage(t, "bukkit:1|c|@0.999999", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:1|c|@0.999999", func() {
 		client := goodClient("")
 		client.Count("bukkit", 1, 0.999999)
 		client.Flush()
@@ -114,7 +86,9 @@ func TestCount(t *testing.T) {
 }
 
 func TestPrefix(t *testing.T) {
-	expectMessage(t, "dude.cool.bukkit:1|c", func() {
+	udp.SetAddr(":8125")
+
+	udp.ShouldReceiveOnly(t, "dude.cool.bukkit:1|c", func() {
 		client := goodClient("dude.")
 		client.Count("cool.bukkit", 1, 1)
 		client.Flush()
@@ -122,12 +96,14 @@ func TestPrefix(t *testing.T) {
 }
 
 func TestTiming(t *testing.T) {
-	expectMessage(t, "bukkit:250|ms", func() {
+	udp.SetAddr(":8125")
+
+	udp.ShouldReceiveOnly(t, "bukkit:250|ms", func() {
 		client := goodClient("")
 		client.Timing("bukkit", 250*time.Millisecond)
 		client.Flush()
 	})
-	expectMessage(t, "bukkit:250000|ms", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:250000|ms", func() {
 		client := goodClient("")
 		client.Timing("bukkit", 250*time.Second)
 		client.Flush()
@@ -135,12 +111,14 @@ func TestTiming(t *testing.T) {
 }
 
 func TestCountUnique(t *testing.T) {
-	expectMessage(t, "bukkit:foo|s", func() {
+	udp.SetAddr(":8125")
+
+	udp.ShouldReceiveOnly(t, "bukkit:foo|s", func() {
 		client := goodClient("")
 		client.CountUnique("bukkit", "foo")
 		client.Flush()
 	})
-	expectMessage(t, "bukkit:foo_bar_1_baz_biz|s", func() {
+	udp.ShouldReceiveOnly(t, "bukkit:foo_bar_1_baz_biz|s", func() {
 		client := goodClient("")
 		client.CountUnique("bukkit", "foo:bar -1- baz|biz")
 		client.Flush()
@@ -148,7 +126,17 @@ func TestCountUnique(t *testing.T) {
 }
 
 func TestBuffer(t *testing.T) {
-	expectMessage(t, "a:1|c\nb:2|c\nc:3|c", func() {
+	udp.SetAddr(":8125")
+
+	udp.ShouldNotReceive(t, "b:2|c", func() {
+		client := goodClient("")
+		client.Count("a", 1, 1)
+		client.Flush()
+		client.Count("b", 2, 1)
+		client.Count("c", 3, 1)
+	})
+
+	udp.ShouldReceiveOnly(t, "a:1|c\nb:2|c\nc:3|c", func() {
 		client := goodClient("")
 		client.Count("a", 1, 1)
 		client.Count("b", 2, 1)
@@ -170,15 +158,11 @@ four.score.and.seven.years.ago:10|c
 four.score.and.seven.years.ago:11|c
 four.score.and.seven.years.ago:12|c
 four.score.and.seven.years.ago:13|c`
-	message := getMessage(func() {
+	udp.ShouldReceiveOnly(t, largePacket, func() {
 		client := goodClient("")
 		for i := 0; i < 16; i++ {
 			client.Count("four.score.and.seven.years.ago", float64(i), 1)
 		}
 		client.Flush()
 	})
-	if message != largePacket {
-		t.Errorf("Expected %d bytes, but got %d", len(largePacket), len(message))
-		t.Error(message)
-	}
 }

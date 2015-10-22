50966c8c873b50ca8381af4d642ea842a7bb0ed7
Tyson Tate <tyson@tysontate.com>
2015-10-22 13:24:05 -0700

    Remove Join().
    Didn't need this after all.

--- name.go
+++ name.go
@@ -6,17 +6,6 @@ import (
 	"github.com/stvp/slug"
 )
 
-// Split takes a valid Statsd metric name and returns it's constituent parts.
-func Split(name string) (parts []string) {
-	parts = []string{}
-	for _, part := range strings.Split(name, ".") {
-		if len(part) > 0 {
-			parts = append(parts, part)
-		}
-	}
-	return
-}
-
 // Join takes a slice of strings (a metric "path") and returns a valid Statsd
 // metric name. It cleans all parts by slug-ifying them.
 func Join(parts []string) (name string) {
--- name_test.go
+++ name_test.go
@@ -1,28 +1,6 @@
 package statsd
 
-import (
-	"reflect"
-	"testing"
-)
-
-func TestSplit(t *testing.T) {
-	tests := []struct {
-		name   string
-		expect []string
-	}{
-		{"", []string{}},
-		{"a", []string{"a"}},
-		{"a.b.c", []string{"a", "b", "c"}},
-		{".a..c.", []string{"a", "c"}},
-	}
-
-	for i, test := range tests {
-		got := Split(test.name)
-		if !reflect.DeepEqual(got, test.expect) {
-			t.Errorf("tests[%d]: expected: %#v, got: %#v", i, test.expect, got)
-		}
-	}
-}
+import "testing"
 
 func TestJoin(t *testing.T) {
 	tests := []struct {
